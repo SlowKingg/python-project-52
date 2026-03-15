ad78b1ba39c2e3651af665bb60946dddc96f770c
Kirill "SlowKing" Nagaqcev <slowkinggg@vk.com>
2026-03-15 19:06:23 +0700

    Add internationalization support for status management templates and messages

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
     name = models.CharField(
         max_length=255,
         unique=True,
-        verbose_name='Имя',
+        verbose_name=_('Name'),
         error_messages={
-            'unique': 'уже существует',
+            'unique': _('already exists'),
         },
     )
     created_at = models.DateTimeField(auto_now_add=True)
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .forms import StatusForm
@@ -22,13 +23,13 @@ class StatusCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('statuses_index')
 
     def form_valid(self, form):
-        messages.success(self.request, 'Статус успешно создан')
+        messages.success(self.request, _('Status has been created successfully'))
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Создать статус'
-        context['submit_label'] = 'Создать'
+        context['title'] = _('Create status')
+        context['submit_label'] = _('Create')
         return context
 
 
@@ -39,13 +40,13 @@ class StatusUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('statuses_index')
 
     def form_valid(self, form):
-        messages.success(self.request, 'Статус успешно изменен')
+        messages.success(self.request, _('Status has been updated successfully'))
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Изменение статуса'
-        context['submit_label'] = 'Изменить'
+        context['title'] = _('Update status')
+        context['submit_label'] = _('Update')
         return context
 
 
@@ -58,10 +59,10 @@ class StatusDeleteView(LoginRequiredMixin, DeleteView):
         try:
             response = super().form_valid(form)
         except ProtectedError:
-            messages.error(self.request, 'Невозможно удалить статус')
+            messages.error(self.request, _('Cannot delete status'))
             return redirect('statuses_index')
 
-        messages.success(self.request, 'Статус успешно удален')
+        messages.success(self.request, _('Status has been deleted successfully'))
         return response
 
 
